d1e0a98496ebd2a7d87c810574d5db8af26dbfa6
maylakirana2605-ui <maylakirana2605@gmail.com>
2026-09-04 09:23:48 +0700

    Langkah 3

--- Jobsheet2/oop-praktikum/src/id/ac/polinema/Rectangle.java
+++ Jobsheet2/oop-praktikum/src/id/ac/polinema/Rectangle.java
@@ -4,4 +4,11 @@ public class Rectangle {
     int width;
     int height;
 
+    int area() {
+        return width * height;
+    }
+
+    int perimeter() {
+        return 2 * (width + height);
+    }
 }
