b98ca007af932c425453580e86b23e7681eb8eb4
maylakirana2605-ui <maylakirana2605@gmail.com>
2026-09-09 12:36:25 +0700

    Langkah 7

--- prak/oop-praktikum/src/id/ac/polinema/Main.java
+++ prak/oop-praktikum/src/id/ac/polinema/Main.java
@@ -2,13 +2,14 @@ package id.ac.polinema;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle original = new Rectangle(6, 4);
-        System.out.println("Area: " + original.area());
+        Rectangle[] shapes = new Rectangle[3];
+        shapes[0] = new Rectangle(6, 4);
+        shapes[1] = new Rectangle(3, 3);
+        shapes[2] = new Rectangle(8, 2);
 
-        Rectangle copy = original;
-        copy.width = 10;
-        System.out.println("Via original: " + original.area());
-        System.out.println("Via copy: " + copy.area());
+        for (Rectangle r : shapes) {
+            System.out.println("Area: " + r.area() + ", Perimeter: " + r.perimeter());
+        }
 
         Student s = new Student("Nadia", "S001", 3.8);
         System.out.println(s.describe());
