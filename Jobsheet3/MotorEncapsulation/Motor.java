b1e7d17be0cf4ab41ac81c544e104fde891c8e87
maylakirana2605-ui <maylakirana2605@gmail.com>
2026-09-11 09:47:23 +0700

    Percobaan 2

--- Jobsheet3/MotorEncapsulation/Motor.java
+++ Jobsheet3/MotorEncapsulation/Motor.java
@@ -1,16 +1,40 @@
 package Jobsheet3.MotorEncapsulation;
 
 public class Motor {
-    public int kecepatan = 0;
-    public boolean kontakOn = false;
+    private int kecepatan = 0;
+    private boolean kontakOn = false;
 
-    public void printStatus(){
-        if (kontakOn == true){
-            System.out.println("Kontak On");
+    public void nyalakanMesin() {
+        kontakOn = true;
+    }
+
+    public void matikanMesin() {
+        kontakOn = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan() {
+        if (kontakOn == true) {
+            kecepatan += 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa bertambah karena Mesin Off! \n");
         }
-        else{
+    }
+
+    public void kurangiKecepatan() {
+        if (kontakOn == true) {
+            kecepatan -= 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa berkurang karena Mesin Off! \n");
+        }
+    }
+
+    public void printStatus() {
+        if (kontakOn == true) {
+            System.out.println("Kontak On");
+        } else {
             System.out.println("Kontak Off");
         }
-        System.out.println("Kecepatan " + kecepatan+"\n");
+        System.out.println("Kecepatan " + kecepatan + "\n");
     }
 }
