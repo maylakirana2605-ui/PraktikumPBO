994ce2ef83bd57907c07be98c8a3b8e01f7551e0
maylakirana2605-ui <maylakirana2605@gmail.com>
2026-09-16 16:44:28 +0700

    Tugas no 3

--- Jobsheet3/Tugas/EncapTest.java
+++ Jobsheet3/Tugas/EncapTest.java
@@ -4,9 +4,15 @@ public class EncapTest {
     public static void main(String[] args) {
         EncapDemo encap = new EncapDemo();
         encap.setName("James");
-        encap.setAge(35);
 
+        encap.setAge(35);
         System.out.println("Name: " + encap.getName());
-        System.out.println("Age: " + encap.getAge());
+        System.out.println("Age (input 35): " + encap.getAge());
+
+        encap.setAge(15);
+        System.out.println("Age (input 15): " + encap.getAge());
+
+        encap.setAge(24);
+        System.out.println("Age (input 24): " + encap.getAge());
     }
 }
