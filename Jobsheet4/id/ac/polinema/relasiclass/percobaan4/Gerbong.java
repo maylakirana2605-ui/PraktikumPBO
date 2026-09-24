20e54eb2444194c44e409b29fccba5851861b7ed
maylakirana2605-ui <maylakirana2605@gmail.com>
2026-09-24 21:30:07 +0700

    Modif dari Pertanyaan no 4 Percobaan 4

--- Jobsheet4/id/ac/polinema/relasiclass/percobaan4/Gerbong.java
+++ Jobsheet4/id/ac/polinema/relasiclass/percobaan4/Gerbong.java
@@ -17,7 +17,12 @@ public class Gerbong {
     }
 
     public void setPenumpang(Penumpang penumpang, int nomor) {
-        this.arrayKursi[nomor - 1].setPenumpang(penumpang);
+        if (this.arrayKursi[nomor - 1].getPenumpang() != null) {
+            System.out.println("Peringatan: Kursi nomor " + nomor + " sudah ditempati oleh " 
+                + this.arrayKursi[nomor - 1].getPenumpang().getNama() + "!");
+        } else {
+            this.arrayKursi[nomor - 1].setPenumpang(penumpang);
+        }
     }
 
     public String info() {
--- Jobsheet4/id/ac/polinema/relasiclass/percobaan4/MainPercobaan4.java
+++ Jobsheet4/id/ac/polinema/relasiclass/percobaan4/MainPercobaan4.java
@@ -5,6 +5,10 @@ public class MainPercobaan4 {
         Penumpang p = new Penumpang("12345", "Mr. Krab");
         Gerbong gerbong = new Gerbong("A", 10);
         gerbong.setPenumpang(p, 1);
+
+        Penumpang budi = new Penumpang("67890", "Budi");
+        gerbong.setPenumpang(budi, 1);
+
         System.out.println(gerbong.info());
     }
 }
