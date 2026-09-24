47f4459eb120e926e493f88a5df5fdf42f7837a9
maylakirana2605-ui <maylakirana2605@gmail.com>
2026-09-25 05:53:28 +0700

    Modif dari pertanyaan no 4 percobaan 6

--- Jobsheet4/id/ac/polinema/relasiclass/percobaan6/Laptop.java
+++ Jobsheet4/id/ac/polinema/relasiclass/percobaan6/Laptop.java
@@ -2,13 +2,31 @@ package Jobsheet4.id.ac.polinema.relasiclass.percobaan6;
 
 public class Laptop {
     private String merk;
+    private Printer printerDefault;
 
     public Laptop(String merk) {
         this.merk = merk;
     }
 
-    public void cetakDokumen(Printer printer, String namaFile) {
-        System.out.println(merk + " mengirim dokumen ke printer...");
-        printer.cetak(namaFile);
+    public Laptop(String merk, Printer printerDefault) {
+        this.merk = merk;
+        this.printerDefault = printerDefault;
+    }
+
+    public void setPrinterDefault(Printer printerDefault) {
+        this.printerDefault = printerDefault;
+    }
+
+    public Printer getPrinterDefault() {
+        return printerDefault;
+    }
+
+    public void cetakDokumen(String namaFile) {
+        if (printerDefault != null) {
+            System.out.println(merk + " mengirim dokumen ke printer...");
+            printerDefault.cetak(namaFile);
+        } else {
+            System.out.println("Gagal mencetak: Tidak ada printer default yang terhubung!");
+        }
     }
 }
--- Jobsheet4/id/ac/polinema/relasiclass/percobaan6/MainPercobaan6.java
+++ Jobsheet4/id/ac/polinema/relasiclass/percobaan6/MainPercobaan6.java
@@ -2,8 +2,9 @@ package Jobsheet4.id.ac.polinema.relasiclass.percobaan6;
 
 public class MainPercobaan6 {
     public static void main(String[] args) {
-        Laptop laptop = new Laptop("Thinkpad");
         Printer printer = new Printer("Epson L3110");
-        laptop.cetakDokumen(printer, "Laporan.pdf");
+        Laptop laptop = new Laptop("Thinkpad", printer);
+
+        laptop.cetakDokumen("Laporan.pdf");
     }
 }
